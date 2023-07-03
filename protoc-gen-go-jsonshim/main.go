ee1d580a5b21454562e47530343a097882cd03f2
barry <kooksee@163.com>
2023-07-03 14:05:57 +0800

    fix

--- protoc-gen-go-jsonshim/main.go
+++ protoc-gen-go-jsonshim/main.go
@@ -15,12 +15,12 @@
 package main
 
 import (
-	"google.golang.org/protobuf/types/pluginpb"
 	"path"
 	"strings"
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
