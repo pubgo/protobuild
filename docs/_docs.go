88526338d530a55e05fe86d9e7ed5a1e5d68cd4b
barry <kooksee@163.com>
2025-04-14 21:46:50 +0800

    fix: barry quick fix, 2025-04-14 21:46:50

--- docs/_docs.go
+++ docs/_docs.go
@@ -2,6 +2,8 @@ package docs
 
 // https://github.com/containerd/protobuild
 // https://github.com/bufbuild/buf
+// https://github.com/containerd/protobuild
+// https://github.com/bufbuild/buf
 // https://github.com/bufbuild/protocompile
 // https://github.com/mitchellh/protoc-gen-go-json
 // https://github.com/foxygoat/protog/tree/master/httprule
