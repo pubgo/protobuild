6d25b33ddd23bc7e43b335f7c84e39df9adca52e
barry <kooksee@163.com>
2025-06-29 10:57:20 +0800

    fix: barry quick fix, 2025-06-29 10:57:20

--- cmd/format/lint.go
+++ cmd/format/lint.go
@@ -3,3 +3,5 @@ package format
 // https://github.com/yoheimuta/protolint
 // clang-format -i -style=google role.proto
 // clang-format -i -style=google proto/errorpb/errors.proto
+// https://github.com/googleapis/api-linter
+// buf format
