0f403a3699dbe919e234ce13e6784541ea74cd81
barry <kooksee@163.com>
2025-07-31 21:33:21 +0800

    fix: barry quick fix, 2025-07-31 21:33:21

--- version/version.go
+++ version/version.go
@@ -1,16 +1,3 @@
 package version
 
-import (
-	ver "github.com/hashicorp/go-version"
-	"github.com/pubgo/funk/assert"
-)
-
-var Version = "v0.0.20"
-
-func init() {
-	if Version == "" {
-		Version = "v0.0.1-dev"
-	}
-
-	assert.Exit1(ver.NewVersion(Version))
-}
+var Version = "v0.0.29"
